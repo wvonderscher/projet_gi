36e9426ead91fc4454a088243cf46b7b013ec895
William VONDERSCHER <wvonderscher@gmail.com>
2022-05-10 19:50:06 +0200

    Ajout de l'interface graphique de l'application

--- src/main/java/fr/ul/miage/projet_gi/Main.java
+++ src/main/java/fr/ul/miage/projet_gi/Main.java
@@ -1,10 +1,45 @@
 package fr.ul.miage.projet_gi;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		boolean appEnFonction = true;
+		int operation;
+		
+		while(appEnFonction) {
+			System.out.println("-------------------------------");
+			System.out.println("Parc de recharge, que souhaitez-vous faire?");
+			System.out.println("1 - Se connecter\n2 - Quitter");
+			operation = scannerInt();
+			
+			switch(operation) {
+			case 1:
+				//connexion
+				break;
+			case 2:
+				System.out.println("Au revoir !");
+				appEnFonction = false;
+				break;
+			}
+			
+		}
 	}
-
+	
+	
+	
+	public static int scannerInt() {
+		Scanner sc = new Scanner(System.in);
+		while(!sc.hasNextInt()) {
+			sc.nextLine();
+			System.out.println("entier requis");
+		}
+		int res = sc.nextInt();
+		while(res < 0) {
+			System.out.println("entier ne peut pas être négatif");
+			res = sc.nextInt();
+		}
+		return res;
+	}
 }
