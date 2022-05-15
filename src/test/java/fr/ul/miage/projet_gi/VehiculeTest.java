8b497c4273e50d89403b0dffb36cdf7537d47727
William VONDERSCHER <wvonderscher@gmail.com>
2022-05-15 11:56:51 +0200

    ajout des tests de la methode getVehiculeId

--- src/test/java/fr/ul/miage/projet_gi/VehiculeTest.java
+++ src/test/java/fr/ul/miage/projet_gi/VehiculeTest.java
@@ -5,20 +5,37 @@ import org.junit.jupiter.api.DisplayName;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 @DisplayName("Test Vehicule")
 public class VehiculeTest {
 
 	@DisplayName("Test methode getVehiculeId lorsque le vehicule existe deja")
 	@Test
 	void plaqueExiste() {
-		
+		String plaque = "ZZ-999-ZZ";
+		Connection con = Connexion.getConnexion();
+		Statement insertVehicule;
+		try {
+			insertVehicule = con.createStatement();
+			insertVehicule.executeUpdate("insert into vehicule (marque, immatriculation) values (\"inconnu\", \""+plaque+"\")");
+			assertThat(Vehicule.getVehiculeId(plaque)).isNotEqualTo(-1);
+			insertVehicule = con.createStatement();
+			insertVehicule.executeUpdate("DELETE FROM Vehicule where immatriculation =\""+plaque+"\"");
+			con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	
 	@DisplayName("Test methode getVehiculeId lorsque le vehicule existe pas")
 	@Test
 	void plaqueExistePas() {
-		
+		assertThat(Vehicule.getVehiculeId("ZZ-999-ZZ")).isEqualTo(-1);
 	}
 	
 	@DisplayName("Test methode validePlaque : quans la plaque est invalide")
